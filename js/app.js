2883fe9893bd9639b28441d548d6e20ff24f52dc
Priscilla Pang <Priscilla.pang@keystonelaw.co.uk>
2018-10-27 20:40:55 +0100

    ID number attached and working

--- js/app.js
+++ js/app.js
@@ -4,6 +4,7 @@ $(document).ready(function(){
 			.then(response => response.json())
 			.then(data => display(data.results));
 
+
 		let $card = [];
 		let index = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
 
@@ -22,69 +23,19 @@ $(document).ready(function(){
 		  $card.append(`<li class="cell hide">${user.cell}</li>`);
 			$card.append(`<li class="address hide">${user.location.street}, ${user.location.city}, ${user.location.state} </li>`);
 		 	$card.append(`<li class="dob hide">Birthday: ${createBirthDay(user.dob.date)}</li>`);
-
 			$card.append(`</div>`);
 
-
-
-			function showOverlay() {
-				//run all cards through array
-
-					$card.on('click', function(e){
-
-						let current = e.currentTarget;
-						console.log(current);
-
-
-						let clickedOn = $card.clone(e.currentTarget);
-						$overlay.append(clickedOn);
-						$overlay.removeClass("hide");
-						$('.cell').removeClass("hide");
-						$('.address').removeClass("hide");
-						$('.dob').removeClass("hide");
-
-
-						$('.cross').on('click', function(e){
-
-							$(".overlay .user__card").remove();
-							$overlay.addClass('hide');
-							$('.cell').addClass("hide");
-							$('.address').addClass("hide");
-							$('.dob').addClass("hide");
-
-						});//listener ends
+			console.log($card);
 
 
 
-						$('.right').on('click', function(e){
 
-							clickedOn.remove();
-							$overlay.not('div:last').remove()
-							let nextUser = $card.next('.user__card').clone();
-							console.log(nextUser);
-							$overlay.append(nextUser);
+			});
 
-						});//listener ends
-
-						$('.left').on('click', function(e){
-
-							clickedOn.remove();
-							let prevUser = $card.prev('.user__card').clone();
-							console.log(prevUser);
-							$overlay.append(prevUser);
-
-						});//listener ends
+			//Modal
+			show();
 
 
-					});//listener ends
-
-				}; //function ends
-
-
-
-			showOverlay($card);
-
-	  	});
 
 
 		}//function ends
@@ -93,6 +44,7 @@ $(document).ready(function(){
 
 
 		const $cards = $('.user__card');
+
 		const $directory = $('.list');
 		const $search = $('#search');
 		let $overlay = $('.overlay');
@@ -112,6 +64,103 @@ $(document).ready(function(){
 			min_chars: 3
 		});
 
+//Modal
+
+		function show () {
+			$("div .user__card").on("click", function(e){
+				let currentID = e.target.id;
+				console.log(currentID);
+
+				$overlay.removeClass("hide");
+				$('.cell').removeClass("hide");
+				$('.address').removeClass("hide");
+				$('.dob').removeClass("hide");
+
+				let div = $(this).closest('.user__card').clone(true);
+				div.addClass("active");
+
+				$overlay.append(div);
+
+			});//listener ends
+				left();
+				right();
+
+				close();
+
+		};//function ends
+
+		function left() {
+
+			$('.left').on("click", function(e){
+
+				$(".overlay .user__card").hide();
+				let attached = $(".active").attr("id");
+				let prevID = parseFloat(attached) -1;
+
+
+				if (parseFloat(prevID) === -1) {
+					prevID = 11;
+					parseFloat(prevID);
+
+					let prevDiv = document.getElementById(prevID);
+					console.log(attached);
+					console.log(prevDiv);
+
+				};
+
+
+				// clone(prevDiv).appendTo($overlay);
+
+
+			});//listener ends
+
+		};//function ends
+
+		function right() {
+
+		$('.right').on("click", function(e){
+
+			$(".overlay .user__card").hide();
+			let attached = $(".active").attr("id");
+			let nextID = parseFloat(attached) + 1;
+
+
+			if (parseFloat(nextID) === 12) {
+				nextID = 0;
+				parseFloat(nextID);
+
+				let nextDiv = document.getElementById(nextID);
+				console.log(attached);
+				console.log(nextDiv);
+			};
+
+
+			// clone(nextDiv).appendTo($overlay);
+
+		});//listener ends
+		};//function ends
+
+		function close() {
+			$('.cross').on('click', function(e){
+
+					$(".overlay .user__card").remove();
+					$overlay.addClass('hide');
+					$('.cell').addClass("hide");
+					$('.address').addClass("hide");
+					$('.dob').addClass("hide");
+
+				});//listener ends
+		};//function ends
+
+		function reset() {
+
+			$('.cell').addClass("hide");
+			$('.address').addClass("hide");
+			$('.dob').addClass("hide");
+		};//function ends
+
+
+
 //Birthday
 
 		function createBirthDay(data){
@@ -123,3 +172,31 @@ $(document).ready(function(){
 
 
 });// document ready
+
+
+//
+//
+//
+// 			$('.right').on('click', function(e){
+//
+// 							clickedOn.remove();
+// 							$overlay.not('div:last').remove()
+// 							let nextUser = $card.next('.user__card').clone();
+// 							console.log(nextUser);
+// 							$overlay.append(nextUser);
+// 				});//listener ends
+//
+// 			$('.left').on('click', function(e){
+//
+// 							clickedOn.remove();
+// 							let prevUser = $card.prev('.user__card').clone();
+// 							console.log(prevUser);
+// 							$overlay.append(prevUser);
+//
+// 			 });//listener ends
+//
+// 		 });//function ends
+//
+// 	}//function ends
+//
+// showOverlay();
